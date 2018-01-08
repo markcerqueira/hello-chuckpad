f981e18158b0ef68ca64a36968d284f1d187a10d
Mark Cerqueira <mark.cerqueira@gmail.com>
2018-01-07 20:52:15 -0800

    First pass on PubNub integration with ChuckPadLive

--- hello-chuckpad/View Controllers/LiveViewController.swift
+++ hello-chuckpad/View Controllers/LiveViewController.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
-final class LiveViewController: UIViewController {
+final class LiveViewController: UIViewController, ChuckPadLiveDelegate {
     
     @IBOutlet private var sessionGUIDLabel: UILabel!
+    @IBOutlet private var connectToSessionButton: UIButton!
+    @IBOutlet private var publishMessageButton: UIButton!
     
     private var liveSession: LiveSession?
+    private var firstChuckPadLive: ChuckPadLive?
+
+    // MARK: - View Controller
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sessionGUIDLabel.isHidden = true
+        connectToSessionButton.isHidden = true
+        publishMessageButton.isHidden = true
+    }
     
     // MARK: - IBActions
     
@@ -19,6 +32,34 @@ final class LiveViewController: UIViewController {
         ChuckPadSocial.sharedInstance().createLiveSession("My Live Session") { [weak self] (succeeded, liveSession, error) in
             self?.liveSession = liveSession
             self?.sessionGUIDLabel.text = liveSession?.sessionGUID
+            
+            self?.sessionGUIDLabel.isHidden = false
+            self?.connectToSessionButton.isHidden = false
         }
+    }
+    
+    @IBAction func connectToSessionPressed() {
+        let demoLiveSession = LiveSession()
+        demoLiveSession.sessionGUID = "demo_channel"
+        
+        let chuckPadLive = ChuckPadLive.initWith(demoLiveSession, chuckPadLiveDelegate: self)
+        
+        sessionGUIDLabel.text = "\(demoLiveSession.sessionGUID!) - Connected"
+            
+        publishMessageButton.isHidden = false
+        
+        if firstChuckPadLive == nil {
+            firstChuckPadLive = chuckPadLive
+        }
+    }
+    
+    @IBAction func publishMessagePressed() {
+        firstChuckPadLive?.publish("Hello Spencer!")
+    }
+    
+    // MARK: - ChuckPadLiveDelegate
+    
+    func chuckPadLive(_ chuckPadLive: ChuckPadLive!, didReceive liveStatus: LiveStatus) {
+        print("didReceive - \(liveStatus)")
     }
 }
